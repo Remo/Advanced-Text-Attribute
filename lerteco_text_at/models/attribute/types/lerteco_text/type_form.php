f1fdb128f7dfd67fa8a7bba2ca83ab0f17901548
Remo Laubacher <remo.laubacher@gmail.com>
2013-07-12 21:01:49 +0200

    made package translatable by adding t-function

--- lerteco_text_at/models/attribute/types/lerteco_text/type_form.php
+++ lerteco_text_at/models/attribute/types/lerteco_text/type_form.php
@@ -4,32 +4,32 @@ $fh = Loader::helper('form'); /* @var $fh FormHelper */
 ?>
 
 <fieldset>
-	<legend>Validation</legend>
+	<legend><?php echo t('Validation')?></legend>
 
 	<div class="clearfix control-group">
-		<label class="control-label">Input Type</label>
+		<label class="control-label"><?php echo t('Input Type')?></label>
 		<div class="input controls">
 			<?php echo $fh->select('valType', $typeOptions, $textConfig['valType']); ?>
 		</div>
 	</div>
 	<div id="control-regexp" class="clearfix control-group">
-		<label class="control-label">Regular Expression</label>
+		<label class="control-label"><?php echo t('Regular Expression')?></label>
 		<div class="input controls">
 			<?php echo $fh->text('valRegExp', $textConfig['valRegExp']); ?>
 		</div>
 	</div>
 	<div class="clearfix control-group">
-		<label class="control-label">Required</label>
+		<label class="control-label"><?php echo t('Required')?></label>
 		<div class="input controls">
 			<?php echo $fh->checkbox('valReq', '1', $textConfig['valReq']); ?>
 		</div>
 	</div>
 </fieldset>
 <fieldset id="set-display">
-	<legend>Display</legend>
+	<legend><?php echo t('Display')?></legend>
 	
 	<div id="control-regexp" class="clearfix control-group">
-		<label class="control-label">Format As Type</label>
+		<label class="control-label"><?php echo t('Format As Type')?></label>
 		<div class="input controls">
 			<?php echo $fh->checkbox('formatType', '1', $textConfig['formatType']); ?>
 		</div>
